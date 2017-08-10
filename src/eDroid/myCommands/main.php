9486d33569b9fad2db7dbf2abbc7f0cb8a40a154
eDroiid <edroiiid@gmail.com>
2017-08-10 01:36:26 -0700

    Upload it alllll

---
+++ src/eDroid/myCommands/main.php
@@ -0,0 +1,42 @@
+<?php
+namespace eDroid\myCommands;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use eDroid\myCommands\tasks\getCommandPack;
+use pocketmine\utils\Utils;
+
+class main extends PluginBase {
+	public function onEnable() {
+		$this->getLogger()->info("myCommands is booting up");
+		if(Utils::$online == false) $this->getLogger()->warning("myCommands will not work properly if offline!");
+	}
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+		$cmd = strtolower($command->getName());
+	    if($cmd == "myCommands"){
+	    	if(isset($args[0])){
+	    		if($args[0] == "help"){
+	    			$sender->sendMessage("[myCommands] Run a multiple amount of commands of any lengths with ease!");
+	    			$sender->sendMessage("[myCommands] First head over to http://myCommands.cf/");
+	    			$sender->sendMessage("[myCommands] Then click 'Create Command Pack'");
+	    			$sender->sendMessage("[myCommands] Then add as many commands as you please");
+	    			$sender->sendMessage("[myCommands] Finally press 'Create' and run the command given to you");
+	    		}elseif($args[0] == "run"){
+	    			if(isset($args[1])){
+	    				$sender->sendMessage("[myCommands] Attempting to get command pack: " . $args[1]);
+	    				new getCommandPack($this, $sender, $args[1]);
+	    			}else{
+	    				$sender->sendMessage("[myCommands] Please provide a pack! or do /myCommands help");
+	    			}
+	    		}
+	    	}else{
+	    		$sender->sendMessage("[myCommands] /myCommands < help | run > [args]");
+	    	}
+	    }
+	}
+	public function onDisable() {
+		$this->getLogger()->info("myCommands is shutting down");
+	}
+}
+?>
---
+++ src/eDroid/myCommands/tasks/getCommandPack.php
@@ -0,0 +1,61 @@
+<?php
+namespace eDroid\myCommands\tasks;
+
+use eDroid\myCommands\main as myCommands;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\command\CommandSender;
+use pocketmine\scheduler\AsyncTask;
+
+class getCommandPack extends AsyncTask {
+    private static $api = "http://mycommands.cf/api/check/";
+    private $pack;
+
+    public function __construct(CommandSender $sender, $pack){
+        parent::__construct(["sender" => $sender]);
+        $this->pack = $pack;
+    }
+
+    public function onRun(){
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => self::$api,
+            CURLOPT_POST => 1,
+            CURLOPT_POSTFIELDS => ["pack" => $this->pack]
+        ));
+
+        $res = json_decode(curl_exec($curl), true);
+
+        if(isset($res["commands"])){
+            $this->setResult(array(false, $res["commands"]));
+        }else{
+            $this->setResult(array(true, curl_error($curl)));
+        }
+    }
+
+    public function onCompletion(Server $server){
+        $mcmds = $server->getPluginManager()->getPlugin('myCommands');
+        $result = $this->getResult();
+        $sender = $this->fetchLocal()["sender"];
+
+        if(!$mcmds instanceof myCommands && !$mcmds->isEnabled()) return;
+
+        if($result[0] === true){
+            $mcmds->getLogger()->warning("[myCommands] Failed retrieving command pack. Curl error: " . $this->getResult()[1]); 
+            $sender->sendMessage("[myCommands] Failed retrieving command pack: $this->pack");
+            return;
+        }
+        if($result[0] === false && empty($result[1])){
+            $sender->sendMessage("[myCommands] The '$this->pack' command pack was empty, therefore no commands were ran.");
+            return;
+        }
+
+        foreach($result[1] as $command){
+           $server->dispatchCommand(new ConsoleCommandSender(), $command);
+        }
+
+        $sender->sendMessage("[myCommands] Successfully ran command pack: $this->pack");
+    }
+}
+?>
